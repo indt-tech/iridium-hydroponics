5a18dbb5fd7096330dbc2b10747448dbe8cbf019
jcarlosn <jcarlos.norte@gmail.com>
2024-03-11 01:54:15 +0100

    Adds BentoGroupedCheckbox

--- packages/app/bundles/protobento/index.tsx
+++ packages/app/bundles/protobento/index.tsx
@@ -1,3 +1,4 @@
 export {BentoInput} from './components/BentoInput'
 export {BentoCheckboxCards} from './components/BentoCheckboxCards'
 export {BentoCheckboxList} from './components/BentoCheckboxList'
+export {BentoGroupedCheckbox} from './components/BentoGroupedCheckbox'
