c690e8b0f2dee247c857840db2edfd508ddf3352
MiquelCos <miquel@protofy.xyz>
2024-04-10 12:02:05 +0200

    Updates home screen

--- packages/app/bundles/custom/pages/home.tsx
+++ packages/app/bundles/custom/pages/home.tsx
@@ -1,5 +1,5 @@
 import { Theme, YStack, Text, XStack, Paragraph, SizableText, useThemeName } from "@my/ui";
-import { UIWrapLib, UIWrap, SSR, Image, Spacer, ContainerLarge, BigTitle, LinkGroup, LinkGroupItem, HCenterStack, NextLink, TooltipContainer, DiscordIcon, Section, SpotLight, GithubIcon, ButtonSimple, HorizontalBox, SectionBlock, HoveredGroup, BlockTitle, BackgroundGradient, ElevatedArea, GridElement, RainbowText, FeatureItem, PageGlow, withSession, Page, Grid, useEditor } from "protolib";
+import { UIWrapLib, UIWrap, SSR, Image, Spacer, ContainerLarge, BigTitle, LinkGroup, LinkGroupItem, HCenterStack, NextLink, TooltipContainer, DiscordIcon, Section, SpotLight, GithubIcon, ButtonSimple, HorizontalBox, SectionBlock, HoveredGroup, BlockTitle, BackgroundGradient, ElevatedArea, GridElement, RainbowText, FeatureItem, PageGlow, withSession, Page, Grid, useEditor, Head1, Head3, Head2 } from "protolib";
 import { ChevronRight, Star } from "@tamagui/lucide-icons";
 import Link from "next/link";
 import { DefaultLayout } from "../../../layout/DefaultLayout";
@@ -10,53 +10,49 @@ const isProtected = Protofy("protected", false);
 
 const Home = (props) => {
   const themeName = useThemeName()
-  const logoUrl = "/media/logoFull-" + themeName + ".png"
 
   return (
     <Page id="home-page">
       <DefaultLayout title="Iridium Hydroponics " description="Made with love from Barcelona" footer={false} tintSwitcher={false}>
         <Section>
-          <YStack py="$20" ai="center">
-            <img src={logoUrl} width="650" />
-            <Paragraph color={"$color7"} scale={2} fontWeight="400">
+          <YStack py="$8" ai="center">
+            <Head1 color={"$color7"} my="$1" $gtLg={{ scale: 1.4 }}>
+              Iridium Hydroponic
+            </Head1>
+            <Head3 fontWeight="400">
               Create your own automatized garden
-            </Paragraph>
+            </Head3>
           </YStack>
-        </Section>
-        <Section>
-          <YStack maw="950px" als="center">
+          <YStack gap="$10" maw="950px" als="center">
+            <img src="media/home-cover.png" width="100%" style={{ minHeight: '380px', objectFit: 'cover' }} />
+            <YStack alignSelf="center" position="absolute" top="210px">
+              <Link href="http://localhost:8080/documentation">
+                <ButtonSimple text margin="$13" size="$3" padding="$4" theme="green" alignSelf="center">
+                  go to documentation
+                </ButtonSimple>
+              </Link>
+            </YStack>
             <Grid gap="25" itemMinWidth="280">
               <GridElement title="Modular System">
                 <Text fontWeight="400">A versatile system allowing easy expansion and scalability of hydroponic infrastructure, adapting to farm needs.</Text>
               </GridElement>
               <GridElement title="Precise Monitoring">Equipped with sensors, our system offers precise monitoring of pH, conductivity, and nutrient levels for optimal growth.</GridElement>
               <GridElement title="Smart Farm">Our platform integrates data management enabling analysis and reporting to optimize farm performance, user-friendly for remote control.</GridElement>
-              <img src="/media/electronics.jpg" width="1000" />
             </Grid>
           </YStack>
         </Section>
 
-        <YStack ai={'center'}>
-          <Link href="http://localhost:8080/documentation">
-            <ButtonSimple margin="$13" size="$3" padding="$4" theme="green" alignSelf="center">
-              go to documentation
-            </ButtonSimple>
-          </Link>
-        </YStack>
-
-        <Section pt="$15" borderColor="$color3">
-          <SpotLight />
-          <Theme reset={true}>
+        <Section paddingTop="$10" paddingBottom="$15" borderColor="$color3">
+          <Theme reset={false}>
             <ContainerLarge position="relative">
+              <Head2 fontWeight="400">
+                About the project
+              </Head2>
               <XStack px="$6" pt="$8" gap="$4" $sm={{ flexDirection: "column", px: 0 }}>
                 <YStack w="50%" $sm={{ w: "100%" }}>
                   <YStack gap="$4">
                     <FeatureItem margin="$4" label="We offer">
-                      <Text fontWeight="400">The aim is to</Text>
-                      <Text fontWeight="800">maximize</Text>
-                      <Text fontWeight="400">the use of water and nutrients while increasing crop production per square meter compared to traditional agriculture. This is achieved by</Text>
-                      <Text fontWeight="800">automating monitoring</Text>
-                      <Text fontWeight="400">and maintenance processes to optimize plant growth. Is also easy to operate, even for users with no previous experience in hydroponics.</Text>
+                      <Text fontWeight="400">The aim is to maximize the use of water and nutrients while increasing crop production per square meter compared to traditional agriculture. This is achieved by automating monitoring and maintenance processes to optimize plant growth. Is also easy to operate, even for users with no previous experience in hydroponics.</Text>
                     </FeatureItem>
                     <FeatureItem label="What can you do with it">Access real-time updates on the status of your garden, including data on its progress. Choose between automated cultivation or manual control based on your preference. Learn the fundamentals of hydroponics with open-source resources tailored to your specific needs.</FeatureItem>
                   </YStack>
@@ -86,7 +82,6 @@ export default {
         path: "/packages/app/bundles/custom/pages/home.tsx",
         editors: [],//["admin"],
         context: {
-          logoUrl: "/media/logoFull-light.png"
         },
         components: {
 
