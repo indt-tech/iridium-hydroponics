a99bc99a3d870a00e62eafc36dc764d5bfbc234a
Gerard <gerard@protofy.xyz>
2024-04-10 11:58:14 +0200

    updates links and logos at docs

--- apps/nextra/theme.config.jsx
+++ apps/nextra/theme.config.jsx
@@ -1,25 +1,25 @@
 
-import Image from 'next/image'
-
 
 export default {
   logo: <div style={{ display: 'flex', alignItems: 'center' }}>
     <span style={{ marginRight: '10px' }}>
-      <img src="/logo.png" alt="Protofy" width="40" height="40" />
+      <img src="/logo-simple.png" alt="Protofy" width="40" height="40" />
     </span>
-    <span style={{ fontWeight: 'bold', fontSize: '18px', opacity:0.8}}>Protofy</span>
+    <span style={{ fontWeight: 'bold', fontSize: '18px', opacity:0.8}}>Iridium Hydroponics</span>
   </div>,
   project: {
-    link: 'https://github.com/Protofy-xyz/Protofy'
+    link: 'https://github.com/Protofy-xyz/iridium-hydroponics'
   },
-  docsRepositoryBase: 'https://github.com/Protofy-xyz/Protofy/tree/main/apps/nextra',
-  footer: { text: (
-    <span>
-      {new Date().getFullYear()} ©{' '}
-      <a href="https://protofy.xyz" target="_blank">
-        Protofy
-      </a>
-    </span>
-  )}
+  docsRepositoryBase: 'https://github.com/Protofy-xyz/iridium-hydroponics/tree/main/apps/nextra',
+  footer: {
+    text: (
+      <span>
+        {new Date().getFullYear()} ©{' '}
+        <a href="https://protofy.xyz" target="_blank">
+          Iridium Hydroponics
+        </a>
+      </span>
+    )
+  }
   // ... other theme options
 }
