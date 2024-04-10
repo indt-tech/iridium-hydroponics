336d79b3fd2e41d5d5dd877d676ebf09f826496c
MiquelCos <miquel@protofy.xyz>
2024-04-10 10:48:23 +0200

    Adds media

--- packages/app/layout/HeaderContents.tsx
+++ packages/app/layout/HeaderContents.tsx
@@ -1,16 +1,17 @@
-import { HeaderContents as ProtoHeaderContents, HeaderContentsProps, HeaderLink, HeadAnchor, useSession, ConnectionIndicator, Tinted } from 'protolib'
-import { Text } from 'tamagui'
-import { useAtom } from 'jotai'
-import { Paragraph, Theme, XStack } from '@my/ui';
-import { Cloud, CloudOff } from '@tamagui/lucide-icons';
+import { HeaderContents as ProtoHeaderContents, HeaderContentsProps, HeaderLink, Tinted } from 'protolib'
+import { XStack, useThemeName } from '@my/ui';
 import dynamic from 'next/dynamic';
 
 export const HeaderContents = (props: HeaderContentsProps & { headerTitle?: string }) => {
   //@ts-ignore
   const SessionInfo = dynamic(() => import('./SessionInfo'), { ssr: false })
-
+  const themeName = useThemeName()
   return <ProtoHeaderContents
-    logo={<Paragraph mr={"$2"}><Text fontSize={20} fontWeight={"400"}>{props.headerTitle ?? 'Protofy'}</Text></Paragraph>}
+    logo={
+      <XStack>
+        <img src={'media/logoFull-' + themeName + '.png'} style={{ height: 'auto', width: '100px' }}></img>
+      </XStack>
+    }
     rightArea={<XStack ai="center">
       {props.topBar}
       <XStack $xs={{ display: 'none' }}>
